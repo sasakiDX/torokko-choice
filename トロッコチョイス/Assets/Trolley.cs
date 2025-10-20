ddd9df4a216331c7dcecae91e96533749c0f43aa
Rihito178 <240160@jc-21.jp>
2025-10-20 11:36:32 +0900

    switch文形式を作成 (実行前)

--- トロッコチョイス/Assets/Trolley.cs
+++ トロッコチョイス/Assets/Trolley.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor.SearchService;
+using UnityEngine;
 using UnityEngine.InputSystem.Switch;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +9,24 @@ public class TrolleyChoice : MonoBehaviour
 {
     [Header("移動設定")]
     // public float moveDistance = 3f; // 上下の移動幅（1回の往復距離）
-    public float RidSpeed = 5f;     // 移動速度
+    public float RidSpeed = 5f;     // 移動速度(たまに反映されないため要確認)
+
+
+    enum Scene
+    {     
+        Look,// 停止中
+        Move,     // レール上で移動中
+        Question    // 分岐に入った（イベント発生）
+
+    }
+
+    private Scene state = Scene.Look; // 現在の状態
+
 
     private Vector2 startPos;       // 初期位置
    // private int direction = 1;      // 進む方向（右:1, 左:-1）
     private int  isHitBox = 0;       // レール接触中のカウント
-    private int  isChange = 0;       // Change 接触中かどうか
+    private int  isChange = 0;       // 分岐に接触中
 
     private BoxCollider2D HitBox;   // 当たり判定
 
@@ -25,35 +38,46 @@ public class TrolleyChoice : MonoBehaviour
 
     void Update()
     {
-       
-        //考えただけ
-        /*switch(true)
-        {
-            case bool isHitBox:
 
-                transform.Translate(Vector2.right * RidSpeed * Time.deltaTime);
+        //考えただけ
+        switch (state)
+        {
+            case Scene.Look:
+                //if()//特定の動作をしたら移動が開始する
+                state = Scene.Move;
+                break;
+
+            case Scene.Move:
+                transform.Translate(Vector2.right * RidSpeed * Time.deltaTime);//移動中
+                if (isChange > 0)
+                {
+                    state = Scene.Question;
+                }
+                break;
+
+            case Scene.Question:
+                SceneManager.LoadScene("Question");//                   
+                state = Scene.Move;
+                //GameManager.Instance.state = GameState.Move; // 状態をMoveにセット（通知）
+                //SceneManager.LoadScene("MainScene");         // 実際にMainSceneに切り替える
 
                 break;
 
+
         }
-        */
 
 
-        // レールに触れている間だけ動作
-       //if (isHitBox > 0)
-       //{
-         
-            transform.Translate(Vector2.right * RidSpeed * Time.deltaTime);
-       //}
+        //transform.Translate(Vector2.right * RidSpeed * Time.deltaTime);
+        //レールに触れている間だけ動作
 
+        ///*
+        //if (isChange > 0)
+        //{
+        //    Scene.Question();//イベントシーンへ移行
 
-        // Change に触れたときにシーン移動
-        if (isChange>0)
-        {
-            Debug.Log("Change に触れた！シーン移動");
-            SceneManager.LoadScene("NextScene"); // 今後追加予定のシーン名
-            isChange = 0; // 一度処理したらフラグを戻す
-        }
+        //}
+        //*/
+
     }
 
 
